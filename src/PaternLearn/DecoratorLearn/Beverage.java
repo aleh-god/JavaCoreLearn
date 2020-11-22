883264f7e355eb9783cbff9ba0e5756fcff3135e
aleh-god <aleh.god@gmail.com>
2020-11-22 23:20:56 +0300

    last commit
    last commit

--- src/PaternLearn/DecoratorLearn/Beverage.java
+++ src/PaternLearn/DecoratorLearn/Beverage.java
@@ -1,6 +1,7 @@
 package PaternLearn.DecoratorLearn;
 
 public abstract class Beverage {
+    //Вместо стринг можно вставить Array с которым можно проводить сложные манипуляции
     String description = "Null beverage";
 
     public String getDescription() {
--- src/PaternLearn/DecoratorLearn/CondimentDecorator.java
+++ src/PaternLearn/DecoratorLearn/CondimentDecorator.java
@@ -2,5 +2,6 @@ package PaternLearn.DecoratorLearn;
 
 // Абстрактный класс-декоратор. Наследуем от основного абстрактного класса
 public abstract class CondimentDecorator extends Beverage {
+    //Вместо стринг можно вставить Array с которым можно проводить сложные манипуляции
     public abstract String getDescription();
 }
